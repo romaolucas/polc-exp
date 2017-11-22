788d4a1efb41c3ce62a7b4f3cdcff64909df1311
Lucas Romão <lucas.romao.silva@gmail.com>
2017-11-21 22:04:12 -0200

    fazendo ridge regression

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -4,14 +4,13 @@ This module trains a given dataset (X, t) using logistic regression.
 '''
 
 import numpy as np
+from sklearn.base import BaseEstimator
 
-class LogRegClassifier:
+class LogRegClassifier(BaseEstimator):
 
-    def __init__(self, X, t, thres=10**-4):
-        self.X = X
-        self.t = t
+    def __init__(self, lamb=1e-4, thres=10**-4):
+        self.lamb = lamb
         self.thres = thres
-        self.w = np.zeros(X.shape[1] + 1)
 
     '''
     calculates sigmoid for given value in R
@@ -30,12 +29,13 @@ class LogRegClassifier:
         for i in range(0, N):
             y = self.sigmoid(np.dot(self.w, self.X[i]))
             err = err + (self.t[i]*np.log(y) + (1 - self.t[i])*np.log(1 - y))
+        err -= 0.5*self.lamb*np.dot(self.w, self.w)
         err = err * - 1.0
         return err
 
     def err_grad(self):
         y = np.array([self.sigmoid(np.dot(x, self.w)) for x in self.X])
-        grad = np.dot(self.X.T, (y - self.t))
+        grad = np.dot(self.X.T, (y - self.t)) + self.lamb*self.w
         return grad 
 
     def calc_R_mat(self):
@@ -47,24 +47,22 @@ class LogRegClassifier:
     Trains the model for logistic regression using
     the iterative reweighted least square methods (source: Bishop's book)
     '''
-    def train(self):
+    def fit(self, X, t):
+        self.X = X
+        self.t = t
         self.X = np.hstack((np.ones((self.X.shape[0], 1)), self.X))
+        self.w = np.zeros(X.shape[1] + 1)
+        n, m = self.X.shape
         iterations = 0
         converged = False
         err = self.err_func()
         while not converged:
             R = self.calc_R_mat()
-            R_inv = np.linalg.inv(R)
-            Y = np.array([self.sigmoid(np.dot(x, self.w)) for x in self.X])
-            z = np.dot(self.X, self.w) - np.dot(R_inv, (Y - self.t))
-            # update w using the formula
-            # w = (X'RX)^-1*X'Rz
-            aux1 = np.matmul(self.X.T, R)
-            aux2 = np.matmul(aux1, self.X)
-            aux2 = np.linalg.inv(aux2)
-            self.w = np.dot(aux2, np.dot(aux1, z))
+            grad = self.err_grad()
+            XR = np.dot(self.X.T, R)
+            Hessian = np.dot(XR, self.X) + self.lamb*np.identity(m)
+            self.w = self.w - np.linalg.solve(Hessian, grad)
             updated_err = self.err_func()
-            print("err = {}, updated_err = {}, err - updated_err = {}".format(err, updated_err, err - updated_err))
             converged = (err - updated_err) < self.thres
             err = updated_err
             iterations += 1
@@ -76,31 +74,35 @@ class LogRegClassifier:
         Y = np.array([y >= (1 - y) for y in Y]).astype(int)
         return Y
 
-def calc_accuracy(Y, T):
-    correctly_classified = 0
-    for y, t in zip(Y, T):
-        if y == t:
-            correctly_classified += 1
-    return correctly_classified / Y.shape[0]
+    def score(self, X, t):
+        y = self.predict(X)
+        correctly_classified = 0
+        for y_i, t_i in zip(y, t):
+            if y_i == t_i:
+                correctly_classified += 1
+        return correctly_classified / y.shape[0]
 
-X = []
-t = []
-import csv
-with open('clean2.data', mode='r') as csvfile:
-    data_reader = csv.reader(csvfile, delimiter=',')
-    for row in data_reader:
-        X.append(row[0:166])
-        t.append(row[-1])
+def main():
+    X = []
+    t = []
+    import csv
+    with open('clean2.data', mode='r') as csvfile:
+        data_reader = csv.reader(csvfile, delimiter=',')
+        for row in data_reader:
+            X.append(row[0:166])
+            t.append(row[-1])
 
-X = np.array(X, dtype=float)
-t = np.array(t, dtype=float)
+    X = np.array(X, dtype=float)
+    t = np.array(t, dtype=float)
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, t_train, t_test = train_test_split(X, t, test_size=0.33)
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, t_train, t_test = train_test_split(X, t, test_size=0.33)
 
-print("Treinando modelo")
-classifier = LogRegClassifier(X_train, t_train)
-classifier.train()
-print("Rodando no conjunto de teste")
-Y = classifier.predict(X_test)
-print("Acuracia = ", calc_accuracy(Y, t_test))
+    print("Treinando modelo")
+    classifier = LogRegClassifier()
+    classifier.fit(X_train, t_train)
+    print("Rodando no conjunto de teste")
+    print("Acuracia = ", classifier.score(X_test, t_test))
+
+if __name__ == "__main__":
+    main()
